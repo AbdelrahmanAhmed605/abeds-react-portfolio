53f30e20371e418424e50ec3c77fd8d60256fcbf
AbdelrahmanAhmed605 <abdelrahman.ahmed605@hotmail.com>
2024-04-07 18:27:29 +0400

    Changed about me section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function About() {
   return (
@@ -8,22 +8,39 @@ function About() {
           Abdelrahman <span className="text-primary">Ahmed</span>
         </h1>
         <div className="subheading mb-5">
-          Toronto, ON · (236) 866-2132 ·{" "}
+          Toronto, ON · (236) 866-2132 ·{' '}
           <a href="mailto:abdelrahman.ahmed605@hotmail.com">
             abdelrahman.ahmed605@hotmail.com
           </a>
         </div>
-        <p className="mb-5">
-          I am a Full Stack Developer with a solid skill set in multiple web
-          languages and frameworks. Currently, I am concluding a coding boot
-          camp to enhance my skills and build a robust project portfolio. As a
-          quick learner with a strong work ethic, I am constantly pushing myself
-          to learn new concepts and stay up-to-date with the latest
-          technologies. I thrive in collaborative environments and enjoy taking
-          a leadership role when needed. In the development process, I am a
-          resilient problem-solver who uses critical thinking to overcome any
-          obstacle that comes my way
-        </p>
+        <div>
+          <p className="mb-3">
+            Hey there! I'm Abdelrahman Ahmed, a Full Stack Developer driven by a
+            passion for tackling challenges, embracing learning, and fostering
+            personal growth. With a background in Mechanical Engineering from
+            the University of British Columbia, I made a leap into the world of
+            web development by completing a rigorous Full Stack Web Development
+            Boot Camp at the University of Toronto.
+          </p>
+          <p className="mb-3">
+            After completing the Boot Camp, I gained a solid foundation in
+            various web languages and frameworks, setting the stage for my
+            career journey.
+          </p>
+          <p className="mb-3">
+            Post-boot camp, I dove into solo projects and sought out online
+            courses to further hone my skills and stay up-to-date with industry
+            trends. This ongoing pursuit of knowledge reflects my genuine
+            passion for growth and improvement.
+          </p>
+          <p className="mb-5">
+            Joining HomeAmie as a Full Stack Software Developer provided me with
+            the opportunity to put my skills to the test in a real-world
+            environment. Working alongside a collaborative team, I contributed
+            to the development of practical solutions, leveraging my technical
+            abilities to make meaningful contributions.
+          </p>
+        </div>
       </div>
     </div>
   );
